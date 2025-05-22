45046ea865f67409e0d56e7817c7be1c69607f47
Damon A. Heath <daheath2007@gmail.com>
2025-05-22 09:54:25 -0500

    Update

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,7 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 
 ENV PYTHONUNBUFFERED=1
-ENV PORT=5000
 
-CMD ["gunicorn", "backend.wsgi"]
+EXPOSE 5000
+
+CMD ["sh", "-c", "backend.wsgi:application --bind 0.0.0.0:$PORT"]
--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
+# Name of your Heroku app
 APP_NAME="twiinz-beard-backend"
-HEROKU_IMAGE_TAG="registry.heroku.com/$APP_NAME/web"
 
 echo "Logging into Heroku container registry..."
 heroku container:login
 
-echo "Setting Heroku stack to container..."
-heroku stack:set container -a $APP_NAME
+echo "Building new Docker image..."
+docker build --no-cache -t main:latest .
 
-echo "Building new Docker image (linux/amd64) directly with Heroku tag..."
-docker build --platform=linux/amd64 --no-cache -t $HEROKU_IMAGE_TAG .
+echo "Tagging Docker image..."
+docker tag main:latest registry.heroku.com/$APP_NAME/web
 
-echo "Pushing Docker image..."
-docker push $HEROKU_IMAGE_TAG 
+echo "Pushing Docker image to Heroku..."
+docker push registry.heroku.com/$APP_NAME/web
 
 echo "Releasing Docker container..."
 heroku container:release web -a $APP_NAME
+
+echo "Checking process status..."
+heroku ps -a $APP_NAME
+
+#echo "Opening APP..."
+#heroku open -a $APP_NAME
 
 echo "Tailing logs..."
 heroku logs --tail -a $APP_NAME
