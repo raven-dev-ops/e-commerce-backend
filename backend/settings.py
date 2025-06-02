81b5627cfe92cb08e2f2115859fb1d35cc42f2d8
gptfleet <gptfleet@gmail.com>
2025-06-02 00:56:48 +0000

    Update

--- backend/settings.py
+++ backend/settings.py
@@ -19,6 +19,7 @@ DEBUG = True
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,.herokuapp.com').split(',')
 
 INSTALLED_APPS = [
+    'corsheaders',  # --- Add CORS at the top
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -117,7 +118,21 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-CORS_ALLOWED_ORIGINS = ["https://twiinz-beard-frontend.netlify.app"]
+# --- CORS SETTINGS ---
+CORS_ALLOWED_ORIGINS = [
+    "https://twiinz-beard-frontend.netlify.app",
+]
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # --- Make sure this is first!
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
 LOGGING = {
     'version': 1,
